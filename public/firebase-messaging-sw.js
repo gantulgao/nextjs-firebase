271dd12af5674c9b8182a95f52f600f501852609
ganaa <gantulga.o@gmail.com>
2022-12-25 20:11:15 +0800

    init

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,12 +2,14 @@ importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "your_api_key",
-  authDomain: "your_auth_domain",
-  projectId: "your_project_id",
-  storageBucket: "your_storage_bucket",
-  messagingSenderId: "your_messagin_sender_id",
-  appId: "your_app_id",
+    apiKey: "AIzaSyAGfPP1n6x32nPUuzvKtFisfYQjf2vCYlI",
+    authDomain: "nextjs-test-messaging.firebaseapp.com",
+    projectId: "nextjs-test-messaging",
+    storageBucket: "nextjs-test-messaging.appspot.com",
+    messagingSenderId: "412613513342",
+    appId: "1:412613513342:web:63e3a4a12f64c2c516c82f",
 });
 
 const messaging = firebase.messaging();
+
+
--- utils/firebase.js
+++ utils/firebase.js
@@ -2,47 +2,56 @@ import "firebase/messaging";
 import firebase from "firebase/app";
 import localforage from "localforage";
 
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyAGfPP1n6x32nPUuzvKtFisfYQjf2vCYlI",
+    authDomain: "nextjs-test-messaging.firebaseapp.com",
+    projectId: "nextjs-test-messaging",
+    storageBucket: "nextjs-test-messaging.appspot.com",
+    messagingSenderId: "412613513342",
+    appId: "1:412613513342:web:63e3a4a12f64c2c516c82f",
+    measurementId: "G-6S89XD589X"
+  });
+}
+
 const firebaseCloudMessaging = {
   init: async () => {
-    if (!firebase?.apps?.length) {
-      // Initialize the Firebase app with the credentials
-      firebase?.initializeApp({
-        apiKey: "your_api_key",
-        authDomain: "your_auth_domain",
-        projectId: "your_project_id",
-        storageBucket: "your_storage_bucket",
-        messagingSenderId: "your_messagin_sender_id",
-        appId: "your_app_id",
-      });
+    const messaging = firebase.messaging();
+    try {
 
-      try {
-        const messaging = firebase.messaging();
-        const tokenInLocalForage = await localforage.getItem("fcm_token");
+      const tokenInLocalForage = await localforage.getItem("fff_qqqq");
 
-        // Return the token if it is alredy in our local storage
-        if (tokenInLocalForage !== null) {
-          return tokenInLocalForage;
-        }
-
-        // Request the push notification permission from browser
-        const status = await Notification.requestPermission();
-        if (status && status === "granted") {
-          // Get new token from Firebase
-          const fcm_token = await messaging.getToken({
-            vapidKey: "your_web_push_certificate_key_pair",
-          });
-
-          // Set token in our local storage
-          if (fcm_token) {
-            localforage.setItem("fcm_token", fcm_token);
-            return fcm_token;
-          }
-        }
-      } catch (error) {
-        console.error(error);
-        return null;
+      // Return the token if it is alredy in our local storage
+      if (tokenInLocalForage !== null) {
+        return tokenInLocalForage;
       }
+
+      // Request the push notification permission from browser
+      const status = await Notification.requestPermission();
+      if (status && status === "granted") {
+        // Get new token from Firebase
+        const fcm_token = await messaging.getToken({
+          vapidKey: "BDaNbkFz-AZFlOkozo5LZuqXXh9c4gSOcGcmBPVREUMMyGB_5seQ_QPD5nYtYt8mxRXJQ7ffZuBfQJoslo8MrHc",
+        });
+        console.log("Token:",fcm_token);
+        // Set token in our local storage
+        if (fcm_token) {
+          localforage.setItem("fff_qqqq", fcm_token);
+          return fcm_token;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      return null;
     }
   },
 };
 export { firebaseCloudMessaging };
+
+export const onMessageListener = () => {
+  return new Promise((resolve) => {    
+    
+  });
+  
+}
+ 
